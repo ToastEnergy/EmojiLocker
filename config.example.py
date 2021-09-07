93b2d120031106e0c6d880e366e0968e27394b68
chickenmatty <44543605+chickenmatty@users.noreply.github.com>
2021-09-07 18:55:01 +0200

    added example config

---
+++ config.example.py
@@ -0,0 +1,16 @@
+discord_token = ""
+prefix = "e?"
+support_server = ""
+no_embeds_message = "Please consider giving me the embed links permission to see nicer messages"
+
+# logs
+error_webhook = ""
+commands_webhook = ""
+
+
+# database
+host = ""
+port = 5432
+user = ""
+database = ""
+password = ""
